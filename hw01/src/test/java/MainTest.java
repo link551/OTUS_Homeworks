d3f0db78361dfe53086c2fa709294a4624b76c6c
link551 <link23051992@yandex.ru>
2019-07-24 02:58:13 +0300

    -

---
+++ hw01/src/test/java/MainTest.java
@@ -0,0 +1,12 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainTest {
+
+    @Test
+    public void main() {
+        Assert.assertTrue(true);
+    }
+}
